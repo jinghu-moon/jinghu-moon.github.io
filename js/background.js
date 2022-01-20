249f3ca853ffc69f1d7020fdabd73be5fbafd503
ziguanqing <2027004326@qq.com>
2022-01-20 08:20:45 +0800

    Site updated: 2022-01-20 08:20:44

--- js/background.js
+++ js/background.js
@@ -1,1 +1,9 @@
-const bannerContainer=$("#banner"),viewBg=$("#web_bg"),bannerMask=$("#banner .mask"),bg=$(bannerContainer).css("background-image");$(viewBg).css("background-image",bg),$(bannerContainer).css("background-image","url()");const color=$(bannerMask).css("background-color");$(bannerMask).css("background-color","rgba(0,0,0,0)"),$(viewBg).css("background-color",color);
+const bannerContainer = $("#banner");
+const viewBg = $("#web_bg");
+const bannerMask = $("#banner .mask");
+const bg = $(bannerContainer).css("background-image");
+$(viewBg).css("background-image", bg);
+$(bannerContainer).css("background-image", "url()");
+const color = $(bannerMask).css("background-color");
+$(bannerMask).css("background-color", `rgba(0,0,0,0)`);
+$(viewBg).css("background-color", color);
